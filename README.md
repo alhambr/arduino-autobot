b193a40c739f9a1cd73c21a019387b1f507bfc07
alhambr <vad.alhambr@gmail.com>
2020-12-08 22:33:24 +0200

    отладка поворотов вправо/влево сервомотора с ультразвуковым датчиком растояния

--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 * [Servo](https://github.com/arduino-libraries/Servo) - упарвление сервомотором
 * [NewPing](https://playground.arduino.cc/Code/NewPing/) - управление ультразвуковым датчиком
 
+## Железо
+* Arduino Nano V3 ATmega328
+* Сервопривод SG90
+* Полноприводное шасси 4WD
+* Драйвер 2-канальный PWM mini MX1508
+* Ультразвуковой датчик расстояния HC-SR04
+* Датчик препятствия
+
--- autobot.ino
+++ autobot.ino
@@ -1,20 +1,20 @@
 #define MAX_SPEED 255  // максимальный сигнал на мотор (max 255)
 #define IN1 2
-#define IN2 3         // IN2 обязательно должен быть ШИМ пином!!!
+#define IN2 3
 #define IN3 10
-#define IN4 11        // IN4 обязательно должен быть ШИМ пином!!!
+#define IN4 11
 #define ECHO_PIN 6
-#define TRIG_PIN 7 
-#define SENSOR_PIN 4 
+#define TRIG_PIN 7
+#define SENSOR_PIN 4
 #define MAX_DISTANCE 200
 
-#include <Servo.h> 
+#include <Servo.h>
 #include "GyverMotor.h"
 #include <NewPing.h>
 GMotor motorL(IN1, IN2);
 GMotor motorR(IN3, IN4);
 NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
-boolean goesForward=false;
+boolean goesForward = false;
 int distance = 100;
 int speedSet = 0;
 int sensorState = 0;
@@ -26,8 +26,8 @@ void setup() {
   motorR.setMode(STOP);
   motorL.setMode(STOP);
   pinMode(SENSOR_PIN, INPUT);
-  myservo.attach(5);  
-  myservo.write(0); 
+  myservo.attach(5);
+  lookForward();
   delay(2000);
   distance = readPing();
   delay(100);
@@ -44,17 +44,18 @@ void loop() {
   int distanceL =  0;
   delay(40);
 
-//  Serial.print(distance);
-//  Serial.print(" cm");
-//  Serial.println();
+
+  Serial.print(distance);
+  Serial.print(" cm");
+  Serial.println();
 
 
   Serial.print(sensorState);
   Serial.print(" sn");
   Serial.println();
 
-  if (sensorState == HIGH) {     
-    if(distance<=25) {
+  if (sensorState == HIGH) {
+    if (distance <= 25) {
       moveStop();
       delay(100);
       moveBackward();
@@ -63,73 +64,85 @@ void loop() {
       delay(200);
       distanceR = lookRight();
       delay(200);
+      lookForward();
+      delay(200);
       distanceL = lookLeft();
       delay(200);
-      
-      if(distanceR>=distanceL) {
+      lookForward();
+      delay(200);
+
+      if (distanceR >= distanceL) {
         delay(100);
         turnRight();
         delay(300);
         moveStop();
         delay(200);
-//        Serial.println("turnRight");
+        Serial.println("turnRight");
       } else {
         delay(100);
         turnLeft();
         delay(300);
         moveStop();
         delay(200);
-//        Serial.println("turnLeft");
-      }    
+        Serial.println("turnLeft");
+      }
     } else {
-//      moveForward();
+      moveForward();
     }
     distance = readPing();
   } else {
-      moveStop();
-      delay(100);
-      moveBackward();
-      delay(300);
-      moveStop();
-      delay(200);
-      distanceR = lookRight();
-      delay(200);
-      distanceL = lookLeft();
-      delay(200);
+    moveStop();
+    delay(100);
+    moveBackward();
+    delay(300);
+    moveStop();
+    delay(200);
+    distanceR = lookRight();
+    delay(200);
+    lookForward();
+    delay(200);
+    distanceL = lookLeft();
+    delay(200);
+    lookForward();
+    delay(200);
   }
   sensorState = digitalRead(SENSOR_PIN);
 }
 
-int readPing() { 
+int readPing() {
   delay(70);
   int cm = sonar.ping_cm();
-  if(cm==0)
+  if (cm == 0)
   {
     cm = 250;
   }
   return cm;
 }
 
+int lookForward()
+{
+  myservo.write(90);
+  delay(200);
+}
+
 int lookRight()
 {
-    myservo.write(50); 
-    delay(500);
-    int distance = readPing();
-    delay(100);
-    myservo.write(115); 
-    return distance;
+  myservo.write(10);
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  return distance;
 }
 
 int lookLeft()
 {
-    myservo.write(170); 
-    delay(500);
-    int distance = readPing();
-    delay(100);
-    myservo.write(115); 
-    return distance;
-    delay(100);
+  myservo.write(180);
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  return distance;
 }
+
 
 void moveStop() {
   motorR.setMode(STOP);
@@ -137,11 +150,11 @@ void moveStop() {
 }
 
 void moveForward() {
-  if(!goesForward) {
-    goesForward=true;
+  if (!goesForward) {
+    goesForward = true;
     motorR.setMode(FORWARD);
-    motorL.setMode(FORWARD);    
-    for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) {
+    motorL.setMode(FORWARD);
+    for (speedSet = 0; speedSet < MAX_SPEED; speedSet += 2) {
       motorR.setSpeed(speedSet);
       motorL.setSpeed(speedSet);
       delay(5);
@@ -150,32 +163,32 @@ void moveForward() {
 }
 
 void moveBackward() {
-  goesForward=false;
+  goesForward = false;
   motorR.setMode(BACKWARD);
-  motorL.setMode(BACKWARD);    
-  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) {
+  motorL.setMode(BACKWARD);
+  for (speedSet = 0; speedSet < MAX_SPEED; speedSet += 2) {
     motorR.setSpeed(speedSet);
     motorL.setSpeed(speedSet);
     delay(5);
   }
-}  
+}
 
 void turnRight() {
   motorL.setMode(BACKWARD);
-  motorR.setMode(FORWARD);    
-  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) {
+  motorR.setMode(FORWARD);
+  for (speedSet = 0; speedSet < MAX_SPEED; speedSet += 2) {
     motorR.setSpeed(speedSet);
     motorL.setSpeed(speedSet);
     delay(5);
-  }  
-} 
- 
+  }
+}
+
 void turnLeft() {
   motorR.setMode(BACKWARD);
-  motorL.setMode(FORWARD);    
-  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) {
+  motorL.setMode(FORWARD);
+  for (speedSet = 0; speedSet < MAX_SPEED; speedSet += 2) {
     motorR.setSpeed(speedSet);
     motorL.setSpeed(speedSet);
     delay(5);
-  }   
+  }
 }
